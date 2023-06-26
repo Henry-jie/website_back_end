bc3048ef2210c353d011b46b3afc338f142c202a
Henry-jie <henry_jie@qq.com>
2023-06-27 01:06:31 +0800

    第二次提交，修改了主程序类

--- src/main/java/com/hey/request/WebsiteApplication.java
+++ src/main/java/com/hey/request/WebsiteApplication.java
@@ -8,9 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @MapperScan("com.hey.request.system.mapper")
 public class WebsiteApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(WebsiteApplication.class, args);
   }
-
 }
